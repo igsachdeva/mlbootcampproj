d9a86f0763ac2b34c9f76631df0e9a202c8aeef3
Gautam <gsachdeva2720@gmail.com>
2024-06-05 12:28:54 +0530

    file updated

--- setup.py
+++ setup.py
@@ -1,2 +1,24 @@
-import sys
-print(sys.version)
+from setuptools import setup, find_packages
+from typing import List
+
+hyp="-e ."
+
+def get_requirement(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if hyp in requirements:
+            requirements.remove(hyp)
+        
+    return requirements
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='sunny',
+    author_email='kuchbhi@gmail.com',
+    install_requires=get_requirement("requirements.txt"),
+    packages=find_packages()
+)
